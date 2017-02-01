5215006a91212f9738d7a432b004c0b368f475c2
Ryan <ryancmeinke@gmail.com>
2017-02-01 00:55:47 -0500

    Add files via upload

---
+++ training/buffer_overflow/0/0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buffer[489];
+    int g;
+    gets(buffer);
+    if(g)
+    {
+        printf("Your first trainning is complete operative.\n");
+    }
+
+}
+
---
+++ training/buffer_overflow/0/0_secure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buffer[489];
+    int g;
+    scanf(buffer, stdin, sizeof(buffer));
+    if(g)
+    {
+        printf("Your first trainning is complete operative.\n");
+    }
+
+}
+
